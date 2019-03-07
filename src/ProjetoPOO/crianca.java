8973ea55716f472b0754bbdb20a6415c83126c79
04027387 <tiagomoraes224@gmail.com>
2019-03-07 20:30:56 -0300

    Iniciação de POO( Classe Criança)

---
+++ src/ProjetoPOO/crianca.java
@@ -0,0 +1,21 @@
+package ProjetoPOO;
+
+public class crianca {
+
+    String corCabelo;
+    String corPele;
+    double altura;
+    int idade;
+
+    public static void main( String [] args){
+    }
+
+    void brincar (){
+        System.out.println(" Brincando ...");
+    }
+
+    void correr(double metros){
+        System.out.println("Correndo "+ metros + "metros");
+    }
+
+}
