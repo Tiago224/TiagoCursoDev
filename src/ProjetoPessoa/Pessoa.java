1ebd618dfefebb8e61e66335882ec89daee7002d
04027387 <tiagomoraes224@gmail.com>
2019-03-28 20:45:34 -0300

    Classe Pessoa ( Construtor , get e set) versão 2

--- src/ProjetoPessoa/Pessoa.java
+++ src/ProjetoPessoa/Pessoa.java
@@ -7,4 +7,20 @@ public class Pessoa {
     public Pessoa() {
         System.out.println(" Iniciando...");
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
