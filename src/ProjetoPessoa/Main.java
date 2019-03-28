dc79f74b62cddcdf634cb4f79c8eba6a8be8157a
04027387 <tiagomoraes224@gmail.com>
2019-03-28 20:40:36 -0300

    Classe Pessoa ( Construtor , get e set)

---
+++ src/ProjetoPessoa/Main.java
@@ -0,0 +1,10 @@
+package ProjetoPessoa;
+
+public class Main {
+    public static void main( String [] args){
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        Pessoa p3 = new Pessoa();
+    }
+
+}
---
+++ src/ProjetoPessoa/Pessoa.java
@@ -0,0 +1,10 @@
+package ProjetoPessoa;
+
+public class Pessoa {
+    String nome;
+    String cpf;
+
+    public Pessoa() {
+        System.out.println(" Iniciando...");
+    }
+}
